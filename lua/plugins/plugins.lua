643fbffd19f8140f00cdba2863fa9721b5774afa
Iqbal Hakim <iqbal@ruangguru.com>
2025-01-26 22:06:36 +0700

    show hidden file

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -9,6 +9,25 @@
 -- * override the configuration of LazyVim plugins
 return {
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+    visible = true,
+    show_hidden_count = true,
+    hide_dotfiles = false,
+    hide_gitignored = true,
+    hide_by_name = {
+      -- '.git',
+      -- '.DS_Store',
+      -- 'thumbs.db',
+    },
+    never_show = {},
+        },
+      }
+    }
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = { ensure_installed = { "svelte" } },
   },
